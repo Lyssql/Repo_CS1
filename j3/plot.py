fa0a96f2677846dd65be898f9f73d0b667354625
Uly$$e <ulysse.garnier.lfgeb@gmail.com>
2024-09-18 17:57:58 +0200

    change stff3

--- j3/plot.py
+++ j3/plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
-plt.show()
+plt.plot([np.sin(1), 2, 3, 4])
+plt.ylabel('random numbers')
+plt.savefig("test.png")
